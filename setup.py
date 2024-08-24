5f1da74075502f79b694430a68a3922e3d05aecb
Devon Copeland <devonkcc@gmail.com>
2024-08-24 16:59:02 -0700

    Auto versioning with gh action

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
+import json
 from setuptools import setup, find_packages
 
+# Load the version from version.json
+with open('version.json', 'r') as f:
+    version_json = json.load(f)
+    version = version_json['version']
+
 setup(
-    name='openteleop',
-    version='0.0.7',
+    name="openteleop",
+    version=version,
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'openteleop=openteleop.main:main',
-        ],
-    },
+    description="Python SDK for OpenTeleop",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/openteleop/openteleop-py",
+    author="Devon Copeland",
+    author_email="founders@serial.io",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+    ],
+    install_requires=["requests"],
+    include_package_data=True
 )
