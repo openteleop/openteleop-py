fb58a7e153ea4947d680524aef40d273450f3d1b
Devon Copeland <devonkcc@gmail.com>
2024-08-24 16:42:38 -0700

    Added steps to publish.yml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openteleop',
-    version='0.0.2',
+    version='0.0.6',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
